018f691caa8dc04eb98e05a3b8c9b08e3de0caa3
Anthony Licea <47396431+jinXus135@users.noreply.github.com>
2022-01-19 13:35:35 -0800

    certificate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SigninPage from './pages/signin';
+import CertificationPage from './pages/certification';
 import Home from './pages';
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
          <Route path = "https://jinxus135.github.io/Shipping-React-App/" component = {Home} exact />
          <Route path = "/signin" component = {SigninPage} exact />
          <Route path = "/jinXus135/signin" component = {SigninPage} exact />
+         <Route path = "/certificate" component = {CertificationPage} exact />
           </Switch>
          </Router>
        
